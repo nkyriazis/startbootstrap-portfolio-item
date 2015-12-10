e034c50ca101db9e5d76938482dde89a795d42c6
Nikolaos Kyriazis <nkyriazis@gmail.com>
2015-12-10 11:48:47 +0200

    customize bibtexbrowser

--- bib.inc.php
+++ bib.inc.php
@@ -1,10 +1,4 @@
 <?php
-@define('BIBTEXBROWSER_BIBTEX_LINKS',false);
-@define('BIBTEXBROWSER_DOI_LINKS',false);
-@define('BIBTEXBROWSER_GSID_LINKS',false);
-@define('BIBTEXBROWSER_LAYOUT','list');
-@define('ABBRV_TYPE','none');
-@define('BIBTEXBROWSER_BIBTEX_VIEW', 'reconstructed');
 $_GET['library']=1;
 require_once('bibtexbrowser.php');
 global $db;
---
+++ bibtexbrowser.local.php
@@ -0,0 +1,9 @@
+<?php
+define('BIBLIOGRAPHYSECTIONS','DefaultBibliographySections');
+define('BIBTEXBROWSER_BIBTEX_LINKS',false);
+define('BIBTEXBROWSER_DOI_LINKS',false);
+define('BIBTEXBROWSER_GSID_LINKS',false);
+define('BIBTEXBROWSER_LAYOUT','list');
+define('ABBRV_TYPE','none');
+define('BIBTEXBROWSER_BIBTEX_VIEW', 'reconstructed');
+?>
