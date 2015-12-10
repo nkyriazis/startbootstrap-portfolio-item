2a4586cac400b96bbb85c1d2cc291f6d1b3e0c4f
Nikolaos Kyriazis <nkyriazis@gmail.com>
2015-12-10 12:33:14 +0200

    ...

--- bibtexbrowser.local.php
+++ bibtexbrowser.local.php
@@ -3,7 +3,7 @@ define('BIBLIOGRAPHYSECTIONS','MySectionStyles');
 define('BIBTEXBROWSER_BIBTEX_LINKS',false);
 define('BIBTEXBROWSER_DOI_LINKS',false);
 define('BIBTEXBROWSER_GSID_LINKS',false);
-define('BIBTEXBROWSER_LAYOUT','list');
+//define('BIBTEXBROWSER_LAYOUT','list');
 define('ABBRV_TYPE','none');
 define('BIBTEXBROWSER_BIBTEX_VIEW', 'reconstructed');
 
