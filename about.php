9d5bb418425037c5d9e390272ad9ddb0072b9ac2
Nikolaos Kyriazis <nkyriazis@gmail.com>
2015-12-14 11:21:47 +0200

    About

--- about.php
+++ about.php
@@ -5,5 +5,12 @@
 
 <div class="col-md-10">
 	<h2>About</h2>
-    <?php include 'about_static.html' ?>
+	<p>
+			I am currently a postdoctoral researcher at the <a href="http://www.ics.forth.gr/cvrl">Computer Vision and Robotics Laboratory (CVRL)</a> of the <a href="http://www.ics.forth.gr/">Institure of Computer Science (ICS)</a> at the <a href="http://www.forth.gr/">Foundation for Research and Technology-Hellas (FORTH)</a>, working with Professor <a href="http://www.ics.forth.gr/~argyros">A. A. Argyros</a>.</p>
+		<p>
+			I received my BSc, MSc and PhD at the <a href="http://www.csd.uoc.gr">Computer Science Department</a> of the <a href="http://www.uoc.gr">University of Crete</a>, in 2006, 2009 and 2014, respectively. The topic of my MSc was &quot;<em>Context Free Grammar Induction via Observation of Structured Time Processes</em>&quot;. The topic of my PhD and my current field of research is &quot;<em>Computational methods for observing and understanding the interaction of human and robotic systems with objects of their environment</em>&quot;. The area of study was Computational Vision and Machine Learning.</p>
+		<p>
+			Other research interests regard Physics Simulation, Computer Graphics, Software Engineering and Performance Computing. For more information please refer to my academic <a href="media/cv.pdf">CV</a>.</p>
+		<p>
+			I can be contacted through <a href="http://www.google.com/recaptcha/mailhide/d?k=017KWG_KpTYVYRvB7SsTqX-Q==&amp;c=x7_RDsIJelsjbgxumIBQeRqdoAdEVFNrkY1XeUNupEI=" title="Reveal this e-mail address">e-mail</a>.</p>
 </div>
