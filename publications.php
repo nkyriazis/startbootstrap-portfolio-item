d62a3b7eb31ec7c63841beeb407ec3cf31b849d3
Nikolaos Kyriazis <nkyriazis@gmail.com>
2015-12-15 08:12:47 +0200

    Link to google scholar

--- publications.php
+++ publications.php
@@ -7,6 +7,7 @@ ini_set('display_errors', 1);
 <div class="col-md-12">
 	<h2>Publications</h2>
 	<div class="row">
+    <p>Analytics can be found at <a href="https://scholar.google.gr/citations?user=fUsz0D8AAAAJ&hl=en">Google Scholar</a>.</p>
 	<?php
         display_bib($db->bibdb);
 	?>
